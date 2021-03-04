ec670bd4c455b9c5a22b57bf752efd4d169310bf
Tobias Jansen <tobias.jansen@sap.com>
2021-03-04 11:53:25 +0100

    Updated gitignore

--- dist/app.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { callResult } from './interfaces/interfaces';
-export declare class AppService {
-    getHello(): string;
-    getConfig(): any;
-    createPerson(firstName: string, lastName: string, gender: string, birthday: string, biography: string): Promise<callResult>;
-}
