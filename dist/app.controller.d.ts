428685ec645b963a85e11df5a80f9edd29e539c2
Tobias Jansen <tobias.jansen@sap.com>
2021-03-11 10:55:23 +0100

    Changes

--- dist/app.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { AppService } from './app.service';
-import { registerUserQueryProperties, getNutzerQueryProperties } from './app.apiproperties';
-export declare class AppController {
-    private readonly appService;
-    constructor(appService: AppService);
-    getHello(): string;
-    registerUser(registerUserProperties: registerUserQueryProperties): Promise<string>;
-    getUser(getNutzerProperties: getNutzerQueryProperties): Promise<string>;
-}
--- dist/app.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { callResult } from './interfaces/interfaces';
-export declare class AppService {
-    getHello(): string;
-    getConfig(): any;
-    createNutzer(email: string, password: string, firstName: string, lastName: string): Promise<callResult>;
-    createAdresse(nutzerID: number, strasse: string, hausnummer: number, postleitzahl: string, ort: string): Promise<callResult>;
-    createDepot(nutzerID: number): Promise<callResult>;
-    getNutzer(nutzerID: number): Promise<callResult>;
-}
