14a6747d7d89b3311d8362fe0002a6c2146c0240
Tobias Jansen <tobias.jansen@sap.com>
2021-03-27 11:01:23 +0100

    Added backendport to config

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  const serverConfig = require('../config.json')
+
+  await app.listen(serverConfig.backendPort);
 }
 bootstrap();
